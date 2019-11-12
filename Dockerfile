a4e44d291f06d0d40e2cf49ad662d836aacc3f55
Christian Gulden <christian.gulden@fau.de>
2019-11-12 19:33:25 +0000

    feat: basic end-to-end implementation of the notification workflow based on a central FHIR server

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM gradle:5.6.4-jdk11 AS build
+WORKDIR /home/gradle/src
+COPY --chown=gradle:gradle . .
+RUN gradle build --no-daemon --info
+
+FROM openjdk:11-jre-slim
+COPY --from=build /home/gradle/src/build/libs/*.jar /opt/notify.jar
+ARG VERSION=0.0.0
+ENV app.version=${VERSION}
+ENTRYPOINT ["java", "-jar", "/opt/notify.jar"]
---
+++ src/main/java/org/miracum/recruit/notify/config/NotificationConfiguration.java
@@ -0,0 +1,49 @@
+package org.miracum.recruit.notify.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.yaml.snakeyaml.Yaml;
+import org.yaml.snakeyaml.constructor.Constructor;
+
+import javax.annotation.PostConstruct;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+//@ConfigurationProperties(prefix = "rules")
+public class NotificationConfiguration {
+    private static final Logger log = LoggerFactory.getLogger(NotificationConfiguration.class);
+
+    private List<MailNotificationRule> mail = new ArrayList<>();
+
+    @Value("${notificationrules.path}")
+    private String configFilePath;
+
+    @PostConstruct
+    public void init() throws FileNotFoundException {
+        log.info("Reading config file.");
+        var yaml = new Yaml(new Constructor(NotificationConfiguration.class));
+        InputStream inputStream;
+        try {
+            inputStream = new FileInputStream(configFilePath);
+        } catch (FileNotFoundException e) {
+            log.error("Failed to read configuration file.", e);
+            throw e;
+        }
+        NotificationConfiguration config = yaml.load(inputStream);
+        this.mail = config.getMail();
+    }
+
+    public List<MailNotificationRule> getMail() {
+        return mail;
+    }
+
+    public void setMail(List<MailNotificationRule> mail) {
+        this.mail = mail;
+    }
+}
